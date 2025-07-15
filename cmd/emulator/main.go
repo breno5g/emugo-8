9b4ce2cb8b2a48f65c68d2ddcf47330f9fdf6c93
Breno Santos <brenosantos145@gmail.com>
2025-07-15 20:27:14 -0300

    chore: add more roms

--- cmd/emulator/main.go
+++ cmd/emulator/main.go
@@ -240,7 +240,7 @@ func main() {
 	// fmt.Printf("V3: %d, VF: %d", chip.V[3], chip.V[0xF])
 
 	chip.LoadFontSet() // carrega fontes na memória
-	romData, err := os.ReadFile("assets/roms/superpong.ch8")
+	romData, err := os.ReadFile("assets/roms/ibm-logo.ch8")
 	if err != nil {
 		log.Fatal(err)
 	}
